9dab0c565bf2be43f5301a71ac518758db0741b2
Egor Astakhov <noise718@gmail.com>
2021-11-11 18:52:02 +0300

    CLLocationManager and refactoring

--- WeatherApp/Model/NetworkWeatherManager.swift
+++ WeatherApp/Model/NetworkWeatherManager.swift
@@ -6,11 +6,32 @@
 //
 
 import Foundation
+import CoreLocation
 
-struct NetworkWeatherManager {
-    func fetchCurrentWeather(forCity city: String) {
-        // передаем адрес обращения
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)"
+class NetworkWeatherManager {
+
+
+    enum RequestType {
+        case cityName(city: String)
+        case coordinate(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
+    }
+    
+    var onCompletion: ((CurrentWeather) -> Void)?
+    
+    func fetchCurrentWeather(forRequsetType requestType: RequestType) {
+        var urlString = ""
+        switch requestType {
+        case .cityName(let city):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)&units=metric"
+        case .coordinate(let latitude, let longitude):
+            urlString =
+        "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&apikey=\(apiKey)&units=metric"
+        }
+        
+        performRequest(withURLString: urlString)
+    }
+    
+     func performRequest(withURLString urlString: String) {
         // делаем из строки url
         guard let url = URL(string: urlString) else { return }
         // создаем сессию
@@ -18,13 +39,15 @@ struct NetworkWeatherManager {
         // создаем запрос
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-                let currentWrather = self.parseJSON(withData: data)
+                if let currentWeather = self.parseJSON(withData: data) {
+                    self.onCompletion?(currentWeather)
+                }
             }
         }
         task.resume()
     }
     
-    func parseJSON(withData data: Data) -> CurrentWeather? {
+     func parseJSON(withData data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         
         do {
